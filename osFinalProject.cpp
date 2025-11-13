fd1a2513e494015fab9ed4040c5b1eabca80b157
Jordan Carnahan <jordanlcarnahan@gmail.com>
2025-11-13 10:57:35 -0800

    Created File

---
+++ osFinalProject.cpp
@@ -0,0 +1,13 @@
+//Final Project: Thread Scheduling Simulation
+//osFinalProject.cpp
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <chrono>
+#include <random>
+
+using namespace std;
+mutex mtx;
